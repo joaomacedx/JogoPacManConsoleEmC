48e8e5c8250a27f4dcbd515d81a732a96a54e352
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-05-16 20:38:35 -0300

    Implementando pílula para herói

--- foge.h
+++ foge.h
@@ -2,10 +2,18 @@
 #define BAIXO 's'
 #define DIREITA 'd'
 #define ESQUERDA 'a'
+#define BOMBA 'b'
+
 
 void move(char direcao);
-void  fantasmas();
 int acabou();
-int ehdirecao(char direcao);
-int paraondefantasmavai ( int xatual, int yatual, int* xdestino, int* ydestino );
 
+int ehdirecao(char direcao);
+
+void fantasmas();
+
+int praondefantasmavai(int xatual, int yatual, 
+	int* xdestino, int* ydestino);
+
+void explodepilula();
+void explodepilula2(int x, int y, int somax, int somay, int qtd);
--- mapa.c
+++ mapa.c
@@ -1,105 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include  <string.h>
+#include <string.h>
 #include "mapa.h"
 
 
-void copiamapa (MAPA* destino, MAPA* origem){
-	 int i;
-	 int j;
-	 destino->linhas = origem->linhas;
-	 destino->colunas = origem->colunas;
-	 
-	 alocamapa(destino);
-	 	for (i = 0; i < origem->linhas; i++){
-		 	for (j = 0; j < origem->colunas; j++){
-		 		// destino->matriz [i] [j] = origem->matriz [i] [j] (strcpy faz isso )
-		 		strcpy (destino->matriz[i], origem->matriz[i]);
-		 		
-			 }
-	 	}
+void copiamapa(MAPA* destino, MAPA* origem) {
+	destino->linhas = origem->linhas;
+	destino->colunas = origem->colunas;
+
+	alocamapa(destino);
+	 int i ;
+	 for(i = 0; i < origem->linhas; i++) {
+		strcpy(destino->matriz[i], origem->matriz[i]);
+	}
+
 }
 
-void andanomapa(MAPA*a, int xorigem, int yorigem, int xdestino, int ydestino){
-	
-	char personagem = a->matriz[xorigem] [yorigem];
-	a->matriz [xdestino] [ydestino] = personagem;
-	a->matriz [xorigem] [yorigem] = VAZIO;
+void andanomapa(MAPA* m, int xorigem, int yorigem, 
+	int xdestino, int ydestino) {
+
+	char personagem = m->matriz[xorigem][yorigem];
+	m->matriz[xdestino][ydestino] = personagem;
+	m->matriz[xorigem][yorigem] = VAZIO;
 }
 
-int ehvalida(MAPA*a, int x, int y){
- 	 if (x >= a->linhas)
- 	     return 0 ;
-     if (y >= a->colunas)
-	     return 0 ;
- 	     
- 	     
-     return 1;
+int ehvalida(MAPA* m, int x, int y) {
+	if(x >= m->linhas)
+		return 0;
+	if(y >= m->colunas)
+		return 0;
+
+	return 1;
 }
 
-int ehvazia (MAPA*a, int x, int y){
-     return	a->matriz[x] [y] == VAZIO;
+int ehvazia(MAPA* m, int x, int y) {
+	return m->matriz[x][y] == VAZIO;
 }
 
-int encontramapa(MAPA* a,POSICAO* p, char c){
-     int i; 
-	 int j;
-	 int x;
-	 int y;
-	
- 	for (i = 0; i < a->linhas; i++){
-		for ( j = 0; j < a->colunas; j++){ 
-		   if(a->matriz[i] [j] == c){
-		 	p->x = i;
-		 	p->y = j;
-		 	return 1;
-		   }
-		}	
- 	}
+int encontramapa(MAPA* m, POSICAO* p, char c) {
+	int i;
+	int j;
+	for( i = 0; i < m->linhas; i++) {
+		for(j = 0; j < m->colunas; j++) {
+			if(m->matriz[i][j] == c) {
+				p->x = i;
+				p->y = j;
+				return 1;
+			}
+		}
+	}
+
 	return 0;
 }
 
-void liberamapa(MAPA*a){
- 	int i;
- 	
-	 for (i = 0 ; i  < a->linhas; i++){
-         free (a->matriz[i]);
-      }
-      free(a->matriz);
-}
-void alocamapa(MAPA*a){
-	 int i;
-	 
-     a->matriz = malloc(sizeof(char*) * a->linhas);
-         for ( i = 0; i < a->linhas; i++){
-         a->matriz[i] = malloc(sizeof(char) * (a->linhas + 1));
-       }
+int ehparede(MAPA* m, int x, int y) {
+	return m->matriz[x][y] == PAREDE_VERTICAL ||
+		m->matriz[x][y] == PAREDE_HORIZONTAL;
 }
 
-void lemapa(MAPA*a){
-	 int i;
-	 
-	 FILE* f;
-     f = fopen("mapa.txt","r");
-         if ( f==0 ){
-           printf("Erro na leitura do programa\n");
-           exit(1);
-       }
-     fscanf (f, "%d %d", &(a->linhas), &(a->colunas));
-       
-     alocamapa(a);
-        
-         for (i = 0; i < a->linhas ; i++){
-         fscanf(f,"%s", a->matriz[i]);
-         }
-         fclose(f);
+int ehpersonagem(MAPA* m, char personagem, 
+	int x, int y) {
+
+	return m->matriz[x][y] == personagem;
+
 }
-	 
-void imprimemapa(MAPA*a){
+
+int podeandar(MAPA* m, char personagem, int x, int y) {
+	return
+		ehvalida(m, x, y) &&
+		!ehparede(m, x, y) &&
+		!ehpersonagem(m, personagem, x, y);
+}
+
+void liberamapa(MAPA* m) {
+	 int i;
+	 for( i = 0; i < m->linhas; i++) {
+		free(m->matriz[i]);
+	}
+	free(m->matriz);
+}
+
+void alocamapa(MAPA* m) {
+	m->matriz = malloc(sizeof(char*) * m->linhas);
+	int i;
+	for( i = 0; i < m->linhas; i++) {
+		m->matriz[i] = malloc(sizeof(char) * (m->colunas+1));
+	}
+}
+
+void lemapa(MAPA* m) {
+	FILE* f;
+	f = fopen("mapa.txt", "r");
+	if(f == 0) {
+		printf("Erro na leitura do mapa\n");
+		exit(1);
+	}
+
+	fscanf(f, "%d %d", &(m->linhas), &(m->colunas));
+
+	alocamapa(m);
      int i;
-    
-     	for ( i = 0; i <5; i++){
-         printf("%s\n", a->matriz[i]); 
-         }
+ 	for(i = 0; i < 5; i++) {
+		fscanf(f, "%s", m->matriz[i]);
+	}
+	fclose(f);
 }
 
+void imprimemapa(MAPA* m) {
+	int i;
+	for(i = 0; i < m->linhas; i++) {
+		printf("%s\n", m->matriz[i]);
+	}
+}
